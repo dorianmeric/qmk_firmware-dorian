3ce8f6e02647f4eb26ae2fbdefd1ed55e7afaaf9
root <root@SpeedyPC>
2024-04-30 03:14:51 +0100

    edited hyphen

--- users/dorian-mir/miryoku_babel/miryoku_layer_alternatives.h
+++ users/dorian-mir/miryoku_babel/miryoku_layer_alternatives.h
@@ -8,9 +8,10 @@
 #pragma once
 
 #define xxxx  KC_NO // present but not available for use
+#define Y_KEY MT(MOD_RCTL | MOD_RALT | MOD_RSFT,KC_Y)
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
-KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              MT(MOD_RGUI,KC_Y), KC_U,              KC_I,              KC_O,              KC_P,              \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              Y_KEY,             KC_U,              KC_I,              KC_O,              KC_P,              \
 LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
 KC_Z,              ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,\
 U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_TAB), LT(U_MOUSE,KC_SPC), LT(U_SYM,KC_BSPC),  LT(U_NUM,KC_ENT), LT(U_FUN,KC_DEL),  U_NP,              U_NP
